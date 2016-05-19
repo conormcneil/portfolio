1bc45260901cce27a6a8085520db129f39466490
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-05-19 17:32:00 -0600

    removed index.html and routed to index view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 'public');
+  res.render('index', {title: "Conor Kingston"});
 });
 
 module.exports = router;
