d654afffc575820af41a3b05ed693dbe6ff961de
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-07-21 14:30:19 -0600

    toggle mobile menu on click

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,8 +1,10 @@
 app.controller('ProjectController',makeProjectController);
 app.controller('AboutMeController',makeAboutMeController);
+app.controller('HeaderController',makeHeaderController);
 
 makeProjectController.$inject = ['$scope','ProjectService'];
 makeAboutMeController.$inject = ['$scope'];
+makeHeaderController.$inject = ['$scope'];
 
 function makeProjectController($scope,ProjectService){
   $scope.view = {};
@@ -11,4 +13,14 @@ function makeProjectController($scope,ProjectService){
 function makeAboutMeController($scope) {
   $scope.view = {};
   $scope.view.greeting = 'Hi! I\'m Conor!';
+};
+function makeHeaderController($scope) {
+  $scope.view = {};
+  $scope.view.showMenu = false;
+  $scope.view.toggleMenu = function() {
+    console.log('toggle menu');
+    console.log($scope.view.showMenu);
+    $scope.view.showMenu = !$scope.view.showMenu;
+    console.log($scope.view.showMenu);
+  };
 };
