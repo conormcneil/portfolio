a14019c03797189acfafa112fb75f346f25eedc3
Conor Kingston <conorkingston@Conors-MacBook-Pro.local>
2016-07-21 14:20:26 -0600

    styled home page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('projectApp',[]);
+
+// app.config(["$routeProvider","$locationProvider",function($routeProvider,$locationProvider) {
+//   $routeProvider
+//     .when('/',{});
+//     .when('/contact', {
+//       templateUrl: 'partials/contact.html'
+//     })
+//     .otherwise({
+//       redirectTo: '/'
+//     });
+//   $locationProvider.html5Mode(true);
+// }]);
--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -3,7 +3,13 @@ app.service('ProjectService', function() {
     projects: [{
       title: 'Finding Fido',
       url: 'https://findingfido.herokuapp.com/',
-      collaborators: ['Lizzie Szoke','Andrew West'],
+      collaborators: [{
+        name: 'Lizzie Szoke',
+        github: 'https://github.com/lizziesz'
+      },{
+        name: 'Andrew West',
+        github: 'https://github.com/westaa'
+      }],
       screenshot: 'https://res.cloudinary.com/dmuipy77o/image/upload/v1467838950/Screen_Shot_2016-07-06_at_2.59.03_PM_w6z5zj.png',
       description: 'Finding Fido is a project that attempts to help reunite users with lost pets. Losing a pet can be very stressful and putting up posters around your neighborhood can often feel futile and cumbersome. With Finding Fido, users can post pictures and descriptions of lost pets so that others nearby can look out for them. It also allows users to post when they have found a wandering pet. In this case, users can tell other users when they have found an animal that may belong to someone. The goal is to reduce the number of pets sent to animal shelters where they are eventually and tragically euthanized.'
     },
