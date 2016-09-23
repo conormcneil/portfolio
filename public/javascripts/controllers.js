e8bb786f16845a59fd64b8da0b1ee1d344fdd5c9
Conor Kingston <cmkingston21@me.com>
2016-09-23 17:11:13 -0600

    about me

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,18 +1,12 @@
 app.controller('ProjectController',makeProjectController);
-app.controller('AboutMeController',makeAboutMeController);
 app.controller('HeaderController',makeHeaderController);
 
 makeProjectController.$inject = ['$scope','ProjectService'];
-makeAboutMeController.$inject = ['$scope'];
 makeHeaderController.$inject = ['$scope'];
 
 function makeProjectController($scope,ProjectService){
   $scope.view = {};
   $scope.view.projects = ProjectService.projects;
-};
-function makeAboutMeController($scope) {
-  $scope.view = {};
-  $scope.view.greeting = 'Hi! I\'m Conor!';
 };
 function makeHeaderController($scope) {
   $scope.view = {};
