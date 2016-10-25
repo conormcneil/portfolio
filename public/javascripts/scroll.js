9a14cc980ede19d16c26b17eac26c70455bc9fbe
Conor Kingston <cmkingston21@me.com>
2016-10-25 12:25:37 -0700

    mobile nav active scroll

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -6,22 +6,25 @@ $(document).ready(function(){
     var currentScroll = $(this).scrollTop();
     var $currentSection;
 
-    $sections.each(function(){
+    for (var i = $sections.length - 1; i >= 0; i--) {
       // divPosition is the position down the page in px of the current section
-      var divPosition = $(this).offset().top;
+      var divPosition = $($sections[i])[0].offsetTop;
+      var id;
 
-      if( divPosition - 1 < currentScroll + window.innerHeight * .75 ){
-        $currentSection = $(this);
-      };
+      if (divPosition < currentScroll + window.innerHeight * .75) {
+        $currentSection = $($sections[i]);
+        id = $currentSection[0].id
 
-      if ($currentSection) {
-        var id = $currentSection[0].id;
         $('.header-link').removeClass('active');
         $("#"+id+"-link").addClass('active');
-      };
 
-    });
-      
+        $('.header-link-nav').removeClass('active-nav');
+        $("#"+id+"-link-nav").addClass('active-nav');
+
+        break; // break escapes loop before checking divs out of view
+      };
+    };
+
   });
 
 });
