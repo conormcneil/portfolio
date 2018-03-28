c319dc0129c838ba428a4a622c66586771c7bbee
Conor Kingston <cmkingston21@me.com>
2018-03-27 18:51:23 -0700

    extracted directives into plain html

--- src/js/directives.js
+++ src/js/directives.js
@@ -1,20 +1,4 @@
 app
-  .directive('ckHeader',function() {
-    return {
-      templateUrl: 'html/header.html',
-      controller: 'HeaderController'
-    };
-  })
-  .directive('ckNav',function() {
-    return {
-      templateUrl: 'html/nav.html'
-    }
-  })
-  .directive('ckHome',function() {
-    return {
-      templateUrl: '/html/home.html'
-    }
-  })
   .directive('ckProjects',function() {
     return {
       templateUrl: '/html/projects.html',
@@ -25,20 +9,5 @@ app
     return {
       templateUrl: '/html/project-details.html',
       controller: 'ProjectController'
-    }
-  })
-  .directive('ckAboutMe',function() {
-    return {
-      templateUrl: '/html/about-me.html'
-    }
-  })
-  .directive('ckContactMe',function() {
-    return {
-      templateUrl: '/html/contact-me.html'
-    }
-  })
-  .directive('ckFooter',function() {
-    return {
-      templateUrl: '/html/footer.html'
     }
   });
